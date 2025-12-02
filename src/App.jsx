994d4178305d05813350716fed0e9edeed9483dd
Mikhailo254 <WOT.killer.254@gmail.com>
2025-12-02 02:51:52 +0200

    Initial React + Vite + SCSS project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React працює </h1>
+      <p>Перший реальний крок зроблено.</p>
+    </div>
+  );
+}
+
+export default App;
