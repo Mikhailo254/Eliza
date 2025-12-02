9abaaa87542355f4981a79b616749fdc214155e7
Mikhailo254 <WOT.killer.254@gmail.com>
2025-12-02 22:14:13 +0200

    Added nullable styles and tweaked 'TopNav'

--- src/components/layout/TopNav.jsx
+++ src/components/layout/TopNav.jsx
@@ -3,9 +3,12 @@ function TopNav() {
     <nav className="top-nav">
       <button className="top-nav__item">Поля</button>
       <button className="top-nav__item">Культури</button>
-      <button className="top-nav__item">Календар</button>
+      <button className="top-nav__item">Техніка</button>
+      <button className="top-nav__item">Робітники</button>
       <button className="top-nav__item">Ресурси</button>
-      <button className="top-nav__item">Звіти</button>
+      <button className="top-nav__item">Погодні сценарії</button>
+      <button className="top-nav__item">Календар</button>
+      <button className="top-nav__item">Особистий кабінет</button>
     </nav>
   );
 }
